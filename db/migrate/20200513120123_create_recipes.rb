5f6846634e2b4a8bcda2a633e07baa7e85413bf4
yjspoder <injose.joshi@gmail.com>
2020-05-13 17:39:08 +0530

    Add validations to recipe model

--- db/migrate/20200513120123_create_recipes.rb
+++ db/migrate/20200513120123_create_recipes.rb
@@ -3,6 +3,7 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
     create_table :recipes do |t|
       t.string :name
       t.text :ingredients
+      t.text :instructions
       t.string :image
 
       t.timestamps
